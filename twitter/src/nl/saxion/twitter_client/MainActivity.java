94032f11f31ddb805a163cbf00de0cff9a3d33e2
dennisroelvink <dennis.roelvink@planet.nl>
2015-05-15 16:08:11 +0200

    ???
    yoloswag

--- twitter/src/nl/saxion/twitter_client/MainActivity.java
+++ twitter/src/nl/saxion/twitter_client/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		// hallo
+		// hallo2
 		TweetApplication app = (TweetApplication) getApplicationContext();
 		model = app.getModel();
 		
